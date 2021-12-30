dfbd8b9fd652d72751f49448bcc20a65e22c1bbb
truongcpqn2421998 <truongcpqn2421998@gmail.com>
2021-12-30 10:11:47 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {TimelinesComponent} from './timelines/timelines.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'timelines',
+  component: TimelinesComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
